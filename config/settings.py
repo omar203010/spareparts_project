4b87832c163b1d36d8e07a1350a20cc0c492b1d6
omar203010 <omar.m.alanziy@gmail.com>
2025-06-02 17:19:06 +0300

    تحديث requirements.txt وإضافة gunicorn

--- config/settings.py
+++ config/settings.py
@@ -88,4 +88,29 @@ else:
 # إعدادات الملفات الثابتة
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STAT
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# إعدادات الوسائط باستخدام Cloudinary
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+MEDIA_URL = '/media/'
+
+# إعدادات الأمان في حالة الإنتاج
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    X_FRAME_OPTIONS = "DENY"
+
+# الحقل الافتراضي للنماذج
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
